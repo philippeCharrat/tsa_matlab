d8179be7110b2f611ac0796a304f2afb41543161
AxelBruyere <ax.bru99@gmail.com>
2020-10-13 17:22:38 +0200

    Ajout de la fonction ESW.m

---
+++ TP2/ESW.m
@@ -0,0 +1,22 @@
+function ESW(x,N,fenetre_char,M,NOVERLAP,NFFT)
+   
+% ---Initialisation des variables ---
+    x_seq = x(1:N);
+    eval(['WIN=',fenetre_char,'(M)']);
+    f_abs = 0:1/N;
+    
+% ---Création de l'estimateur 3---
+    [gamma_x_c,fabs] = pwelch(x_seq,WIN,NOVERLAP,NFFT,1,'twosided');
+    gamma_x_c = 10 * log10(gamma_x_c);
+    
+% ---DSP moyenne vraie et (gamma(f) * Wbm(f))(f)
+    [Gth,Gbiais,fth]=sptheo(M,'welch','rectwin');
+    
+    
+% ---Partie affichage---
+    figure(2)
+    plot(fabs,gamma_x_c,fth,Gth,'r',fth,Gbiais,'k')
+    axis([0 0.5 -50 10])
+    legend('Estimation de la DSP','DSPMV','Convolution de la DSP et de la fenetre de Barlett')
+    title('Densités spectrales de puissance calculées')    
+end
--- TP2/main.m
+++ TP2/main.m
@@ -4,3 +4,4 @@ s = genbrfil;
 %load('sig.mat');
 %ESS(s,1,10000,2^14);
 %ESM(s,10000,500,512);
+%ESW(s,10000,'rectwin',500,250,512);
