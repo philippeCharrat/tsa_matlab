0e78ee4635efb203491e4df49b40e2eff5ae8196
Axel Bruyere <ax.bru99@gmail.com>
2020-11-03 16:32:42 +0100

    ajout jusqu'à la partie 3 TP4

--- TP4/partie2.m
+++ TP4/partie2.m
@@ -11,7 +11,6 @@ clc;close all;clear variables;
     xlabel('Temps(s)')
     ylabel('Signal reçu')
     title('Signal reçu à restaurer')
-    close all
 %2. Estimation de la fonction d'autocorrélation
     %Initialisation des variables
     K = 200
@@ -32,4 +31,47 @@ clc;close all;clear variables;
     plot(lags,R)
     xlabel('Retards')
     ylabel('Corrélations')
-    title("Fonction d'autocorrélation calculée pour K = 200")    
+    title("Fonction d'autocorrélation calculée pour K = 200") 
+    close all
+%3. Identification du modèle AR(M)
+    %Initialisation des variables
+    M = 20;
+    lag_0 = find(lags==0);
+    %Création du vecteur r pour la fonction toeplitz.m
+    r = zeros(1,M+1);
+    r(1) = R(lag_0);
+    for k = 1:1:M
+        r(k+1) = R(lag_0 + k);
+    end
+    %Création de la matrice de Toeplitz
+    tpz = toeplitz(r);
+    %Pseudo-inversion pour résoudre le système 
+    inv_tpz = pinv(tpz);
+    %Vecteur contenant les résultats du système
+    result = zeros(M+1,1);
+    result(1) = 1;
+    %Matrice des h/sigma^2
+    phi = inv_tpz*result;
+    h = zeros(M+1,1);
+    %Division des termes par phi(1) = 1/sigma^2
+    for k = 1:1:M+1
+        h(k) = -phi(k)/phi(1);
+    end
+    %On tronque le vecteur de son premier terme
+    h = h(2:end);    
+    %Partie affichage
+    figure(4)
+    stem(h)
+    xlabel('Indices k')
+    ylabel('h[k]')
+    title('Coefficients du modèle AR(M) obetnus')
+    
+
+
+
+
+
+
+
+
+
